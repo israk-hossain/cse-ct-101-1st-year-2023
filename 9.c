f21ad717eff6025a3291914f049be191a0e424d7
Israk Hossain <140854414+israk-hossain@users.noreply.github.com>
2023-11-20 22:30:46 +0600

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,16 +1,35 @@
-
 #include <stdio.h>
-#include <string.h>
+
+void mergeStrings(const char *firstString, const char *secondString, char *result) {
+    int i = 0, j = 0;
+
+    while (firstString[i] != '\0') {
+        result[i] = firstString[i];
+        i++;
+    }
+
+    while (secondString[j] != '\0') {
+        result[i] = secondString[j];
+        i++;
+        j++;
+    }
+
+    result[i] = '\0';
+}
 
 int main() {
     char firstString[100], secondString[100];
+    char mergedString[200];
 
     printf("Enter the first string: ");
-    gets(firstString);
+    scanf("%s", firstString);
+
     printf("Enter the second string: ");
-    gets(secondString);
-    strcat(firstString, secondString);
-    printf("Merged string: %s\n", firstString);
+    scanf("%s", secondString);
+
+    mergeStrings(firstString, secondString, mergedString);
+
+    printf("Merged string: %s\n", mergedString);
 
     return 0;
 }
