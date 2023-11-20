0a78c0a2664694177b97bdc1b0f6d9a93845c414
Israk Hossain <140854414+israk-hossain@users.noreply.github.com>
2023-11-21 02:05:19 +0600

    Update 3.c

--- 3.c
+++ 3.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    char str[21];
+    char str[20];
 
     printf("Enter a string (up to 20 characters): ");
-    scanf("%20s", str);
+    scanf("%19s", str);
 
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
