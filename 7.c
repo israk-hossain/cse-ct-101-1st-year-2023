25b978c68a6ae93f786324591c90f098916fdd3c
Israk Hossain <140854414+israk-hossain@users.noreply.github.com>
2023-11-20 22:29:08 +0600

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,6 +1,20 @@
-
 #include <stdio.h>
-#include <string.h>
+
+void concatenateStrings(char firstString[], char secondString[]) {
+    int i = 0, j = 0;
+
+    while (firstString[i] != '\0') {
+        i++;
+    }
+
+    while (secondString[j] != '\0') {
+        firstString[i] = secondString[j];
+        i++;
+        j++;
+    }
+
+    firstString[i] = '\0';
+}
 
 int main() {
     char firstString[100], secondString[100];
@@ -11,7 +25,7 @@ int main() {
     printf("Enter the second string: ");
     gets(secondString);
 
-    strcat(firstString, secondString);
+    concatenateStrings(firstString, secondString);
 
     printf("Concatenated string: %s\n", firstString);
 
