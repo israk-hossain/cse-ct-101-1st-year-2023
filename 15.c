cce24236d361b69e9ae0509d045fb92c902be9c6
Israk Hossain <140854414+israk-hossain@users.noreply.github.com>
2023-11-21 12:25:29 +0600

    Update 15.c

--- 15.c
+++ 15.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    char str[100];
+    int alphabets = 0, digits = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            alphabets++;
+        } else if (str[i] >= '0' && str[i] <= '9') {
+            digits++;
+        }
+    }
+
+    printf("Total alphabets: %d\n", alphabets);
+    printf("Total digits: %d\n", digits);
+
+    return 0;
+}
