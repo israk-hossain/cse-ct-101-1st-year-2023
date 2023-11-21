923aff4d2cba22b9c4484f2f432351978d0be78c
Israk Hossain <140854414+israk-hossain@users.noreply.github.com>
2023-11-21 12:17:47 +0600

    Update 25.c

--- 25.c
+++ 25.c
@@ -1,6 +1,4 @@
-
 #include <stdio.h>
-#include <string.h>
 
 int main() {
     char array1[100];
@@ -13,12 +11,22 @@ int main() {
     printf("Enter the second array: ");
     scanf("%s", array2);
 
-    strcpy(mergedArray, array1);
-    strcat(mergedArray, array2);
+    int i = 0, j = 0;
+
+    while (array1[i] != '\0') {
+        mergedArray[i] = array1[i];
+        i++;
+    }
+
+    while (array2[j] != '\0') {
+        mergedArray[i] = array2[j];
+        i++;
+        j++;
+    }
+
+    mergedArray[i] = '\0';
 
     printf("Merged array: %s\n", mergedArray);
 
     return 0;
 }
-
-/*
